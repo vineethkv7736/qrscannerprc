589c8fde25073e0810e1161b1b72c9f3f2c85e38
vineethkv7736 <vineethkv7736@gmail.com>
2023-11-13 22:52:41 +0530

    qrerror

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import QRCodeScanner from './components/QRCodeScanner'
+
 import PersonData from './components/PersonData'
 
 function App() {
@@ -22,10 +22,6 @@ function App() {
     <div className="App">
       <PersonData/>
       <h1>QR Code Scanner</h1>
-      <button onClick={openScanner}>Open Scanner</button>
-
-      {showScanner && <QRCodeScanner onClose={closeScanner} />}
-      
     </div>
   );
 }
